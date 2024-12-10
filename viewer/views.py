cbcfb3681f854ac7daf71d813f1dd1da9081380f
tanatos09 <tanatos09@gmail.com>
2024-12-10 16:26:07 +0100

    edited templates for product and services

--- viewer/views.py
+++ viewer/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from products.models import Product
+
 
 def home(request):
     return render(request, 'home.html')
@@ -7,8 +9,14 @@ def home(request):
 def products(request):
     return render(request, 'products.html')
 
+def products(request):
+    products = Product.objects.filter(product_type='merchantdise')
+    print(products)
+    return render(request, 'products.html', {'products': products})
+
 def services(request):
-    return render(request, 'services.html')
+    products = Product.objects.filter(product_type='service')
+    return render(request, 'services.html', {"products": products})
 
 def trainers(request):
     return render(request, 'trainers.html')

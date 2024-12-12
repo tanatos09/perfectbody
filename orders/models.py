fbc27bd34b22e5facc75f627375de9e99d2e407c
tanatos09 <tanatos09@gmail.com>
2024-12-12 21:29:12 +0100

    models created

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,47 @@
-from django.db import models
+from django.db.models import Model, ForeignKey, SET_NULL, DateTimeField, DecimalField, PositiveIntegerField, CASCADE, CharField
+from accounts.models import UserProfile, Address
+from products.models import Product
 
-# Create your models here.
+
+class Order(Model):
+    ORDER_STATES = [
+        ('PENDING', 'Čeká na vyřízení'),
+        ('SHIPPING', 'Na cestě'),
+        ('COMPLETED', 'Dokončeno'),
+        ('CANCELED', 'Zrušeno'),
+    ]
+
+    customer = ForeignKey(UserProfile, on_delete=SET_NULL, null=True, blank=True, related_name='orders'    )
+    order_state = CharField(max_length=20, choices=ORDER_STATES, default='PENDING')
+    order_creation_datetime = DateTimeField(auto_now_add=True)
+    total_price = DecimalField(max_digits=10, decimal_places=2)
+    billing_address = ForeignKey(Address, on_delete=SET_NULL, null=True, blank=True, related_name='billing_orders')
+    shipping_address = ForeignKey(Address, on_delete=SET_NULL, null=True, blank=True, related_name='shipping_orders')
+
+    def __repr__(self):
+        customer_str = self.customer.username if self.customer else "GUEST"
+        return (
+            f"Order(id={self.id}, customer={customer_str}, state={self.order_state}, "
+            f"total_price={self.total_price}, created={self.order_creation_datetime})"
+        )
+
+    def __str__(self):
+        customer_str = self.customer.username if self.customer else "GUEST"
+        return f"Order #{self.id} - {customer_str}"
+
+
+class OrderProduct(Model):
+    order = ForeignKey(Order, on_delete=CASCADE, related_name='items')
+    product = ForeignKey(Product, on_delete=CASCADE, related_name='order_items')
+    quantity = PositiveIntegerField()
+    price_per_item = DecimalField(max_digits=10, decimal_places=2)
+
+    def __repr__(self):
+        customer_str = self.order.customer.username if self.order.customer else "GUEST"
+        return (
+            f"OrderProduct(order_id={self.order.id}, customer={customer_str}, product={self.product.product_name}, "
+            f"quantity={self.quantity}, price_per_item={self.price_per_item})"
+        )
+
+    def __str__(self):
+        return f"{self.product.product_name} x {self.quantity} (Order #{self.order.id})"

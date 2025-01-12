a5fcaadb75dcade9fbbe065bc8e562e8b0fb3408
info <info@barasera.net>
2025-01-12 14:17:32 +0100

    products submenu

--- viewer/context_processors.py
+++ viewer/context_processors.py
@@ -1,5 +1,5 @@
-# viewer/context_processors.py
 from products.models import Category
+
 def navbar_context(request):
     main_categories = Category.objects.filter(category_parent=None).prefetch_related('subcategories').distinct()
     return {
